9b685725ae4b8edb429842f6e4277afbda95f1a6
Abishek Satyan <abishek.satyan@gmail.com>
2021-06-23 14:16:22 +0530

    User interface done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { getLocaleDateFormat } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragdropApp';
+  showTodo=false
+  taskForm=this.fb.group({
+    task:['',(Validators.required)]
+  })
+  movies = [];
+  constructor(private fb:FormBuilder) {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event.currentIndex)
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    console.log(event.container.data[event.currentIndex])
+  
+  }
+  getData(){
+    
+    if(!this.taskForm.valid){
+    alert("task field cannot be empty")
+    }
+    else{
+      console.log(this.taskForm.value.task)
+    }
+  }
+  showData(){
+    this.showTodo=!this.showTodo
+  }
 }
